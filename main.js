dde5cdacd2db6702f7ed3ad6ce73c8d1bfbebbfb
luisdasilvahenrique <luisenriklh1177027@gmail.com>
2022-02-12 14:32:11 -0300

    ScrollReveal api

--- main.js
+++ main.js
@@ -42,3 +42,19 @@ const swiper = new Swiper('.swiper', {
     mouseheel: true,
     Keyboard: true
   });
+
+//   ScrollReveal: Mostrar elementos quando der scroll na página
+const scrollReveal = ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 500,
+    reset: true
+})
+
+scrollReveal.reveal(
+    `#home .image, #home .text,
+     #about .image, #about .text,
+     #services header, #services .card,
+     #testimonials header, #testimonials .testimonial,
+     #contact .text, #contact .links`,
+    { interval: 100 })
